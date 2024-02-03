762af9bc4810067712879253d33571f34512986f
RobinKohrs <robin.kohrs@gmx.de>
2024-02-03 14:03:19 +0100

    new Data 2024-02-03 14:02:05

--- analysis/prepare_bathymetry_each_spot/prepare_bathymetry_each_spot.R
+++ analysis/prepare_bathymetry_each_spot/prepare_bathymetry_each_spot.R
@@ -5,12 +5,13 @@ library(sf)
 library(rajudas)
 library(jsonlite)
 library(stars)
+library(exactextractr)
 
 
 # data_spots --------------------------------------------------------------
 path_spots = here("output/all_spots/all_spots.csv")
 data_spots = read_csv(path_spots)
-geo_spots = data_spots %>% st_as_sf(coords=c("lat", "lon"), crs=4326)
+geo_spots = data_spots %>% st_as_sf(coords=c("lon", "lat"), crs=4326)
 
 
 # data bathy --------------------------------------------------------------
@@ -19,5 +20,25 @@ stars_bathy = stars::read_ncdf(path_bathy, proxy = T)
 
 
 # for each point extract 1 degree -----------------------------------------
-buffer_degreees = 1
+buffer_meter = 200000
+for(i in seq_along(1:nrow(geo_spots))){
+  row = geo_spots[i, ]
+  id = row$id
+  buffer = st_buffer(row, dist=buffer_meter)
+
+  # write to tempfile
+  tf = glue("{tempfile()}.geojson")
+  write_sf(buffer, tf)
+
+  # cropped raster path
+  op_cropped_bathy_spot = makePath(here(glue("data_raw/cropped_bathy_spot/bathy_{id}.tif")))
+  if(file.exists(op_cropped_bathy_spot)){
+    next
+  }
+
+  # extract raster
+  cmd = glue("gdalwarp -s_srs 'EPSG:4326' -of GTiff -cutline {tf} -crop_to_cutline {path_bathy} {op_cropped_bathy_spot}")
+  system(cmd)
+
+}
 
